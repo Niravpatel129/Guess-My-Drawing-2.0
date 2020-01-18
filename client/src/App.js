c3f19e04891141a3bc8e3ee0f2de267db16b3344
Niravpatel129 <niravpatelp129@gmail.com>
2020-01-17 19:44:41 -0500

    setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./App.css";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Landing from "./components/Landing/Landing";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Landing />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
