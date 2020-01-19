41f27214475bfc77df5f4b00914efdbb532cf49a
Niravpatel129 <niravpatelp129@gmail.com>
2020-01-19 08:44:24 -0500

    updated user info

--- server/index.js
+++ server/index.js
@@ -45,6 +45,7 @@ io.on("connection", socket => {
 
   // get new chat message
   socket.on("chatMessage", ({ name, room, input }) => {
+    console.log(name);
     AllRooms.pushMessage(name, room, input);
     const messages = AllRooms.getAllMessages(room);
     io.in(room).emit("updateMessage", messages);
