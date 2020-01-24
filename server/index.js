62e3f2b3ae8efb7c00cd9834709f520e68fd21cf
Niravpatel129 <niravpatelp129@gmail.com>
2020-01-24 15:33:14 -0500

    cleanup

--- server/index.js
+++ server/index.js
@@ -11,8 +11,6 @@ const { AllRoomsGenerator } = require("./utils/AllRoomsGenerator");
 
 const AllRooms = new AllRoomsGenerator();
 
-let globalSocket;
-
 server.listen(PORT, () => {
   console.log("Server started on port", PORT);
 });
@@ -20,8 +18,6 @@ server.listen(PORT, () => {
 app.use(router);
 
 io.on("connection", socket => {
-  globalSocket = socket;
-
   socket.emit("connected");
 
   // join room
